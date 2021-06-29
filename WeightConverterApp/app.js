2af5f3d6b36844ad985ba9cf717cacf73ef7e783
mohitsahni96 <mohit.a.sahni@capgemini.com>
2021-06-29 11:36:18 +0530

    chnanges fixed

--- WeightConverterApp/app.js
+++ WeightConverterApp/app.js
@@ -1,9 +1,7 @@
- //  document.getElementById('output').style.visibility = "hidden";
-        document.getElementById('lbsInput').addEventListener('input', function (e) {
-            //  document.getElementById('output').style.visibility = "hidden";
-            let lbs = e.target.value;
-            document.getElementById('gramsOutput').innerHTML = lbs / 0.0022046;
-            document.getElementById('kgOutput').innerHTML = lbs / 2.2046;
-            document.getElementById('ozOutput').innerHTML = lbs * 16;
-            document.getElementById('mgOutput').innerHTML = lbs / 0.0000022046;
-        });
+document.getElementById("lbsInput").addEventListener("input", function(e) {
+  let lbs = e.target.value;
+  document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046;
+  document.getElementById("kgOutput").innerHTML = lbs / 2.2046;
+  document.getElementById("ozOutput").innerHTML = lbs * 16;
+  document.getElementById("mgOutput").innerHTML = lbs / 0.0000022046;
+});
