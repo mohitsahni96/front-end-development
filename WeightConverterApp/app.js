7abbe7d9154b30fd65629931615d9920da7edec5
Mohit Sahni <36255708+mohitsahni96@users.noreply.github.com>
2021-06-30 14:27:46 +0530

    Updated App.js

--- WeightConverterApp/app.js
+++ WeightConverterApp/app.js
@@ -1,3 +1,4 @@
+//DOM MANIPULATION 
 document.getElementById("lbsInput").addEventListener("input", function(e) {
   let lbs = e.target.value;
   document.getElementById("gramsOutput").innerHTML = lbs / 0.0022046;
